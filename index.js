41b24efe31a0e3c88fa3e12bac6140b83b5ee923
Faraz Khan <farazkhanaa2555@gmail.com>
2024-05-28 08:09:19 +0500

    refactor: update code with some changes

--- index.js
+++ index.js
@@ -3,18 +3,38 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+const friends = [
+  {
+    name: "faraz",
+    id: 0,
+  },
+  { name: "rana", id: 1 },
+  { name: "zohaib", id: 2 },
+];
+
 app.get("/", (req, res) => {
   console.log("first");
   return res.send("Hello We are learning");
 });
 
+app.use(express.json());
+
 app.get("/messages", (req, res) => {
-  return res.status(200).send("Hello Testing test");
+  return res.status(200).send("Hello Testing test user");
 });
 
-app.post("/test", (req, res) => {
-  if (req.url === "/test") {
-    return res.send("Pakistan is teh largest country");
+app.post("/friends", (req, res) => {
+  console.log("req ", req.body);
+  if (req.body.name) {
+    friends.push({
+      name: req.body.name,
+      id: friends.length,
+    });
+    return res.send(friends);
+  } else {
+    return res.status(402).send({
+      error: "user not found",
+    });
   }
 });
 
