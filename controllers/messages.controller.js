7bc87a35d6785da8fb59670bf29a2f8395d8f755
Faraz Khan <farazkhanaa2555@gmail.com>
2024-05-31 07:20:06 +0500

    feat: complete express node js project by ZTM

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,9 +1,13 @@
 const path = require("path");
 
 function getMessages(req, res) {
-  res.sendFile(
-    path.join(__dirname, "..", "public", "images", "farazkhanaa.jpg")
-  );
+  res.render("messages", {
+    title: "Messages to my Friends",
+    friend: "Abdul Moeed",
+  });
+  // res.sendFile(
+  //   path.join(__dirname, "..", "public", "images", "farazkhanaa.jpg")
+  // );
 }
 
 function postMessage(req, res) {
